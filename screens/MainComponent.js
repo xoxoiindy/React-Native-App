f8d6066ad63a98254b45ad7510c3681ea056a639
Cindy <proanocindy@gmail.com>
2022-09-02 18:08:08 -0600

    Forms

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -11,6 +11,7 @@ import  logo  from '../assets/images/logo.png'
 import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
+import ReservationScreen from './ReservationScreen';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchPartners } from '../features/partners/partnersSlice';
@@ -108,6 +109,32 @@ const ContactNavigator = () => {
     );
 };
 
+const ReservationNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Reservation'
+                component={ReservationScreen}
+                options={({ navigation }) => ({
+                    title: 'Reservation Search',
+                    headerLeft: () => (    
+                        <Icon
+                            name='tree'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                            
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
+
+
+
 const AboutNavigator = () => {
     const Stack = createStackNavigator();
     return (
@@ -196,6 +223,22 @@ const Main = () => {
                         drawerIcon: ({ color }) => (
                             <Icon
                                 name= 'list'
+                                type= 'font-awesome'
+                                size={24}
+                                iconProps={{width: 10}}
+                                color={color}
+                            />
+                        )
+                    }}
+                />
+                <Drawer.Screen
+                    name='ReserveCampsite'
+                    component={ReservationNavigator}
+                    options={{ 
+                        title: 'Reserve Campsite',
+                        drawerIcon: ({ color }) => (
+                            <Icon
+                                name= 'tree'
                                 type= 'font-awesome'
                                 size={24}
                                 iconProps={{width: 10}}
